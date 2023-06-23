eda8676b88e23570dd27a93cac13305395cb3c6e
Meena <meenakshi6296@gmail.com>
2023-06-23 16:32:55 -0400

    init commit

--- amplify-iot/src/App.js
+++ amplify-iot/src/App.js
@@ -7,29 +7,33 @@ import SiteNav from './components/common/SiteNav';
 import SiteFooter from './components/common/SiteFooter';
 
 import Homepage from './components/home/Homepage';
-import Loginpage from './components/auth/Loginpage';
-import Register from './components/auth/Register';
+
 import Devicedetails from './components/Devices/Devicedetails';
 
-// import { Amplify } from 'aws-amplify';
-// import { Authenticator, View, Image, useTheme, Text  } from '@aws-amplify/ui-react';
-// import awsExports from './aws-exports';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
 
+Amplify.configure(awsExports);
 
 function App() {
   return (
+    <Authenticator loginMechanisms={'username'}>
+      {({signOut, user}) => (
     <div >
-      <SiteNav/>
+      <SiteNav LogOut={signOut}/>
       <Routes>
         <Route path="*" element={<Homepage/>}/>
         <Route path="/" exact={true} element={<Homepage/>}/>
-        <Route path="/Login" element={<Loginpage/>}/>
-        <Route path="/register" element={<Register/>}/>
+       
         <Route path="/Devices" element={<Devicedetails/>}/>
       </Routes> 
       <SiteFooter/>
     </div>
+    )}
+    </Authenticator>
   );
+
 }
 
 export default App;
--- amplify-iot/src/components/home/Homepage.js
+++ amplify-iot/src/components/home/Homepage.js
@@ -1,4 +1,5 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -7,6 +8,7 @@ import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 
 function Homepage(){
+   
 return(
     <Container>
             <Row className="px-4 my-5">
@@ -17,9 +19,11 @@ return(
                 </Col>
                 <Col sm={6}>
                     <h1 className="font-weight-light">IOT App</h1>
-                    <p className="mt-4"></p>                        
-                        <Button variant="outline-primary">get started &gt;&gt;</Button>
-                   
+                    <p className="mt-4"></p>       
+                    <Link to="/Devices">
+                     <Button variant="outline-primary">View Devices &gt;&gt;</Button>
+                </Link>
+
                 </Col>
              </Row>
     </Container>
